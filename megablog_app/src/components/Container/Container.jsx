152a2a050a5dbe50a2da931bdc926a213b314960
Asheesh Sharma <144527342+asheeshkumar933@users.noreply.github.com>
2023-11-09 04:48:01 +0000

    add new file

---
+++ megablog_app/src/components/Container/Container.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+function Container ({ childreb}){
+
+    return (
+
+        <div className=" w-full max-w-7xl mx-auto px-4 ">
+             {Children} </div>;
+
+    )
+    
+
+}
+
+export default Container
