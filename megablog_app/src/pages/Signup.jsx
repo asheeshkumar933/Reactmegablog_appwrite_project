71499a2c2fbdab7c5d1ec9220999795802e8b874
Asheesh Sharma <144527342+asheeshkumar933@users.noreply.github.com>
2023-11-12 05:01:11 +0000

    add pages

---
+++ megablog_app/src/pages/Signup.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Signup as SignupCompnent } from "../components";
+
+
+function Signup (){
+    return (
+        <div className=" py-8">
+            <SignupCompnent /> 
+       </div>
+    )
+}
+
+export default Signup
